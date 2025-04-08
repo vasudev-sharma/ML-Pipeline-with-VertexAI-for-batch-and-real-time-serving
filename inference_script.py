f6971d432731974e94bc9a1254fede764a91e9bc
vasudev-sharma <vasu@cs.toronto.edu>
2025-04-08 15:22:11 -0400

    Add get health check

--- inference_script.py
+++ inference_script.py
@@ -5,13 +5,19 @@ from pydantic import BaseModel
 import numpy as np
 
 app = FastAPI()
+
+
 # model = pickle.load("model.pkl")
 
 class Input(BaseModel):
     instances: list
 
+@app.get("/health_check")
+def health_check():
+    return {"status": "healthy"}
+
 @app.post("/predict")
-def predict(input: Input):
+def predict(input):
     # data = np.array(input.instances)
     preds = [1, 2, 3]
     return {"predictions": preds}
