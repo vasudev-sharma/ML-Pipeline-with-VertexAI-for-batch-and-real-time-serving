e03a2f16750bfce0eb6bd460aaff1e24e7defad7
vasudev-sharma <vasu@cs.toronto.edu>
2025-04-08 15:53:23 -0400

    Compute predictions

--- inference_script.py
+++ inference_script.py
@@ -3,6 +3,7 @@ import pickle
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
+import uvicorn
 
 app = FastAPI()
 
@@ -17,8 +18,18 @@ def health_check():
     return {"status": "healthy"}
 
 @app.post("/predict")
-def predict(input):
+def predict(input: BaseModel):
     # data = np.array(input.instances)
-    preds = [1, 2, 3]
+    preds = [4.60042024e-03, 3.33172912e-01, 5.26700015e+00, 9.20000000e+01,0.00000000e+00, 0.00000000e+00, 2.80000000e+01, 4.00000000e+00,5.10000000e+01]
+    predictions = None
+    with open("model.pkl", 'rb') as file:
+        model = pickle.load("model.pkl")
+        predictions = model(input)
+    # print(model)
+    
+    if predictions:
+        return {"predictions": predictions}
     return {"predictions": preds}
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
