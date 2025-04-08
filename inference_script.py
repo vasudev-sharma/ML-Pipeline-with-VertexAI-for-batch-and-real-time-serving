b7265170ad4d5e3268d17a39462203a1ebe39590
vasudev-sharma <vasu@cs.toronto.edu>
2025-04-08 15:07:47 -0400

    Test infernece script

--- inference_script.py
+++ inference_script.py
@@ -5,13 +5,14 @@ from pydantic import BaseModel
 import numpy as np
 
 app = FastAPI()
-model = pickle.load("model.pkl")
+# model = pickle.load("model.pkl")
 
 class Input(BaseModel):
     instances: list
 
 @app.post("/predict")
 def predict(input: Input):
-    data = np.array(input.instances)
-    preds = model.predict(data).tolist()
+    # data = np.array(input.instances)
+    preds = [1, 2, 3]
     return {"predictions": preds}
+
